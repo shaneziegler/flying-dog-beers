32fe3bcfadd26debaf4c5645f80343d8116dc39a
shaneziegler <56492231+shaneziegler@users.noreply.github.com>
2020-12-16 12:17:27 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
-import dash
-import dash_core_components as dcc
-import dash_html_components as html
-import plotly.graph_objs as go
+
 
 ########### Define your variables
-x = 455
+x = "shanez"
 
 print(x)
